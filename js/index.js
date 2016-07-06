4c4f13a86fd7b256794ffd799331dcfa091d49f6
Billard <foresome1249@gmail.com>
2016-07-06 10:44:09 -1000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-var heroName;
+var STARTING_PAGE;
+var HeroName;
+var HeroAbilites = {};
+
+function CreatePage(tab)
+{
+	p_id = obj.id;
+	p_main_txt = obj.main_text || "Unassigned text";
+	p_head_txt = obj.header || "";
+	p_has_item = obj.has_item || false;
+	p_lft_txt = obj.left_text || "Left text";
+	p_rgt_txt = obj.right_text || "Right text";
+	p_bck_clr = obj.background_color || "white";
+	p_txt_clr = obj.text_color || "";
+	p_giv_itm = obj.give_item || "";
+	p_does_need_itms = obj.does_need_items || false;
+	p_req_itms = obj.need_items || [];
+	return page;
+}
