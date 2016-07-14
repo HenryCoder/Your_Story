7473e401b788d2b31e768e15fd340d8e2a7244a8
Aidan808 <buffblueboy@gmail.com>
2016-07-14 10:58:27 -1000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -165,9 +165,9 @@ function CreatePage(tab)
 // Create default pages, feel free to edit values but do not change object name
 var PAGE_START = new CreatePage({
 	page_id : 1,
-	main_text : "You wake up in a small house. There is a table with an apple, knife, and book on it. You pick up the apple and decide if you are going to take the knife or the book.",
+	main_text : "You wake up in a small house. There is a table with an apple, sword, and book on it. You pick up the apple and decide if you are going to take the knife or the book.",
 	header : "Welcome to Your Story",
-	left_text : "Pick up knife",
+	left_text : "Pick up sword",
 	left_page : "PAGE_0",
 	right_text : "Pick up book",
 	right_page : "PAGE_1"
@@ -419,7 +419,7 @@ var PAGE_0 = new CreatePage({
 	right_text : "Go to church", 
 	right_page : "PAGE_CHURCH",
 	give_strength : 5,
-	give_items : ["Knife"]
+	give_items : ["Sword"]
 });
 var PAGE_1 = new CreatePage({
 	page_id : 3,
