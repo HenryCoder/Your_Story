ef831dd241c5c09b18726001712af4110b0a3992
Str33t_S1gn <buffblueboy@gmail.com>
2018-04-27 11:14:22 -1000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -595,6 +595,13 @@ var PAGE_LOOKUP = new CreatePage({
 	left_page : "PAGE_LOOKDOWN",
 	right_text : "Look Around", 
 	right_page : "PAGE_LOOK"
+	
+function changeBackground(color) {
+   document.body.style.background = color;
+}
+
+<BODY onload="changeBackground('green');">
+				 
 });
 var PAGE_LOOKDOWN = new CreatePage({
 	page_id : 3,
